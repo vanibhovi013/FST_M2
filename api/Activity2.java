03d3f6e8d7f1be3c2a7f3cceeea2cb5e215fc6c2
vanibhovi013 <90038754+vanibhovi013@users.noreply.github.com>
2022-11-08 19:52:22 +0530

    Add files via upload

---
+++ api/Activity2.java
@@ -0,0 +1,81 @@
+package Activities;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import org.testng.annotations.Test;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+public class Activity2 {
+    // Set base URL
+    final static String ROOT_URI = "https://petstore.swagger.io/v2/user";
+
+    @Test(priority=1)
+    public void addNewUserFromFile() throws IOException {
+        // Import JSON file
+        FileInputStream inputJSON = new FileInputStream("src/test/java/Activities/userinfo.json");
+        // Read JSON file as String
+        String reqBody = new String(inputJSON.readAllBytes());
+
+        Response response =
+                given().contentType(ContentType.JSON) // Set headers
+                        .body(reqBody) // Pass request body from file
+                        .when().post(ROOT_URI); // Send POST request
+
+        inputJSON.close();
+
+        // Assertion
+        response.then().body("code", equalTo(200));
+        response.then().body("message", equalTo("1111"));
+    }
+
+    @Test(priority=2)
+    public void getUserInfo() {
+        // Import JSON file to write to
+        File outputJSON = new File("src/test/java/Activities/userGETResponse.json");
+
+        Response response =
+                given().contentType(ContentType.JSON) // Set headers
+                        .pathParam("username", "Hima") // Pass request body from file
+                        .when().get(ROOT_URI + "/{username}"); // Send POST request
+
+        // Get response body
+        String resBody = response.getBody().asPrettyString();
+
+        try {
+            // Create JSON file
+            outputJSON.createNewFile();
+            // Write response body to external file
+            FileWriter writer = new FileWriter(outputJSON.getPath());
+            writer.write(resBody);
+            writer.close();
+        } catch (IOException excp) {
+            excp.printStackTrace();
+        }
+
+        // Assertion
+        response.then().body("id", equalTo(1111));
+        response.then().body("username", equalTo("Hima"));
+        response.then().body("firstName", equalTo("Hima"));
+        response.then().body("lastName", equalTo("Bindu"));
+        response.then().body("email", equalTo("hb@mail.com"));
+        response.then().body("password", equalTo("password123"));
+        response.then().body("phone", equalTo("9812763450"));
+    }
+
+    @Test(priority=3)
+    public void deleteUser() throws IOException {
+        Response response =
+                given().contentType(ContentType.JSON) // Set headers
+                        .pathParam("username", "Hima") // Add path parameter
+                        .when().delete(ROOT_URI + "/{username}"); // Send POST request
+
+        // Assertion
+        response.then().body("code", equalTo(200));
+        response.then().body("message", equalTo("Hima"));
+    }
+}
